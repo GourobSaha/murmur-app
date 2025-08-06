dc2ac8e78a0dfe653c035f510f4060d2baa41d3a
GourobSaha <gourobsaha009@gmail.com>
2025-08-06 21:31:33 +0600

    like basic operations added

--- webapp-test/server/src/modules/likes/likes.controller.ts
+++ webapp-test/server/src/modules/likes/likes.controller.ts
@@ -1,34 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body, Query, Get, ParseIntPipe } from '@nestjs/common';
 import { LikesService } from './likes.service';
-import { CreateLikeDto } from './dto/create-like.dto';
-import { UpdateLikeDto } from './dto/update-like.dto';
+import { ToggleLikeDto } from './dto/create-like.dto';
 
-@Controller('likes')
+@Controller('api/likes')
 export class LikesController {
-  constructor(private readonly likesService: LikesService) {}
+  constructor(private readonly likesService: LikesService) { }
 
-  @Post()
-  create(@Body() createLikeDto: CreateLikeDto) {
-    return this.likesService.create(createLikeDto);
+  @Post('toggle')
+  async toggleLike(@Body() dto: ToggleLikeDto) {
+    return this.likesService.toggleLike(dto.userId, dto.murmurId);
   }
 
-  @Get()
-  findAll() {
-    return this.likesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.likesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLikeDto: UpdateLikeDto) {
-    return this.likesService.update(+id, updateLikeDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.likesService.remove(+id);
+  @Get('count')
+  async countLikes(@Query('murmurId', ParseIntPipe) murmurId: number) {
+    return this.likesService.countLikes(murmurId);
   }
 }
